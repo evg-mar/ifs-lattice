ffca15c4aef11352570a5d56e3c7b2de8a216454
Evgeniy Marinov <marinov.evgeniy@gmail.com>
2016-11-06 19:57:27 +0200

    Added topological operators.

--- ifs_operators_plot.py
+++ ifs_operators_plot.py
@@ -25,6 +25,35 @@ def supPi(*ifsets):
         return None
     else:
         return (mus,nus)
+##############
+## Unary operators
+##############
+def neg(ifset):
+    return (ifset[1], ifset[0])
+##############
+## Topological operators
+##############
+
+def incGeneral(ifset, alpha, beta, gamma):
+    mus = ifset[0]
+#     nsu = ifset[1]
+    mu_slope = 1 / (1-gamma)
+
+    mus_rst = map(lambda mu: mu if(mu >= alpha) else \
+                                max(mu_slope*(mu - gamma*alpha), 0.0),
+               mus)
+    nus_rst = map(lambda pair: pair[1] if pair[1] >= beta else \
+                               min((1-gamma)*pair[1] + gamma*beta, 1 - pair[0]),
+                               zip(*ifset))
+#     print("in incGeneral \n")    
+    return (np.asarray(list(mus_rst)), np.asarray(list(nus_rst)))
+
+def clGeneral(ifset, alpha, beta, gamma):
+#     print("in clGeneral")
+#     print(neg(ifset))
+    result = neg(incGeneral(neg(ifset), beta, alpha, gamma))
+
+    return result
 
 
 
